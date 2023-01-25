00341f3ad6151b302d30069d905baceebff26798
Miguel Vargas <tapion@Miguels-MacBook-Pro.local>
2023-01-25 16:56:38 -0500

    Generics

--- src/app.ts
+++ src/app.ts
@@ -1,131 +1,49 @@
-// Code goes here!
-type option1 = {
-    name: string;
-    privileges: string[];
+//GENERICS
+function merge <T extends object, U extends object>(obj1: T, obj2: U){
+    return Object.assign(obj1, obj2);
 }
 
-type option2 = {
-    name: string;
-    date: Date;
+const obj = merge({name: "Miguel", last: "Vargas"}, {passion: "travel", has: "bike"});
+console.log(obj.passion);
+
+interface Lengthy {
+    length: number;
 }
 
-type general = option1 & option2;
-type general2 = option1 | option2;
-
-const amigo: general = {
-    name: "hola",
-    privileges: ['Otro privilegio'],
-    date: new Date(),
+function lengthAndDescription<T extends Lengthy>(el: T): [T, string]{
+    let description = 'No elements';
+    if(el.length === 1){
+        description = `Got 1 element`;
+    }else if(el.length > 1){
+        description = `Got ${el.length} elements`;
+    }
+    return [el, description];
 }
 
-const amigo1: general2 = {
-    name: "Name amigo1",
-    privileges: ['Otro privilegio'],
-    date: new Date(),
+console.log(lengthAndDescription(["hola","como ", null]))
+
+function extract<T extends object, U extends keyof T>(obj: T, key: U): string {
+    return `Values is ${obj[key]}`;
 }
 
-type opt1 = string | number;
-type opt2 = number | boolean;
+console.log(extract({name: "Miguel"}, "name"));
 
-const vopt1 = 2;
-const vopt11 = 2;
-type opt3 = opt1 & opt2;
-
-const hola: opt3 = 1;
-
-class test1 {
-    doSomth(val: general2) {
-        console.log(val.name);
+class Generico<T extends string | number | boolean>{
+    private data: T[] = [];
+    addItem(val: T){
+        this.data.push(val);
+    }
+    
+    removeItem(val: T){
+        this.data.splice(this.data.indexOf(val), 1);
+    }
+    getItems(){
+        console.log(this.data);
     }
 }
 
-class test2 {
-    doSomth(val: general2) {
-        console.log(val.name);
-    }
-    hello() {
-        console.log('hello');
-    }
-}
-
-type tests = test1 | test2;
-
-const vt1 = new test1();
-const vt2 = new test2();
-
-function testing(val: tests, param: general2) {
-    val.doSomth(param);
-    if (val instanceof test2) {
-        val.hello();
-    }
-}
-
-
-function say(val1: general2) {
-    console.log('Nombre general', val1.name);
-    if ('date' in val1) {
-        console.log('Date general', val1.date);
-    }
-    return;
-}
-
-interface Bird {
-    type: 'bird';
-    flySpeed: number;
-}
-
-interface Horse {
-    type: 'horse';
-    runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-function animales(an: Animal) {
-    let speed;
-    switch (an.type) {
-        case 'bird':
-            speed = an.flySpeed;
-            break;
-        case 'horse':
-            speed = an.runningSpeed;
-            break;
-    }
-    console.log('Moving at this spped ', speed);
-}
-
-console.log('hola amigos', amigo.date)
-console.log(optFun(vopt1, vopt11));
-say(amigo1);
-testing(vt2, amigo1);
-
-animales({type: 'bird', flySpeed: 10});
-
-// const htmlElement = <HTMLParagraphElement>document.getElementById('paragraph-text');
-const htmlElement = document.getElementById('paragraph-text');
-if(htmlElement){
-    (htmlElement as HTMLParagraphElement).innerHTML = "hola";
-
-}
-
-//INDEX PROPERTIES
-interface ErrorMessages {
-    [prop: string]: string;
-}
-
-const errors: ErrorMessages = {
-    textError: "This is an error",
-    screenError: "This is an error two",
-}
-
-//Functions overload
-function optFun(val1: number, val2: number): number;
-function optFun(val1: string, val2: string): string;
-function optFun(val1: opt1, val2: opt1) {
-    if (typeof val1 === 'string' || typeof val2 === 'string') {
-        return val1.toString() + val2.toString();
-    }
-    return val1 + val2;
-}
-
-console.log(optFun("hola ","como te va").split(' '));
+const h1 = new Generico<string>();
+h1.addItem('10');
+h1.removeItem('10');
+h1.addItem('100');
+console.log(h1.getItems());
