9167e7007bcc1c79db543c376de6cbb37740fc7f
Miguel Vargas <tapion@Miguels-MacBook-Pro.local>
2023-03-07 11:47:38 -0500

    Usgin google maps api and types

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
-import { ProjectInput } from './components/project-input';
-import { ProjectList } from './components/project-list';
+import axios from 'axios';
+const form = document.querySelector("form")! as HTMLFormElement;
+const adressInput = document.getElementById("address")! as HTMLInputElement;
+const GOOGLE_API_KEY = 'AIzaSyAeiu7tSQcN3EygYd9J5rLh269MOoei-j8';
 
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
+type GOOGLE_RESPONSE = {
+    results: { geometry: { location: { lat: number, lng: number } } }[]
+}
 
+// declare var google: any;
 
-console.log('holadasdsada')
+function eventHandler(event: Event) {
+    event.preventDefault();
+    const adress = adressInput.value;
+    console.log(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(adress)}&key=${GOOGLE_API_KEY}`);
+    axios.get<GOOGLE_RESPONSE>(`https://maps.googleapis.com/maps/api/geocode/json?address=${adress}&key=${GOOGLE_API_KEY}`)
+    .then(response => {
+        const coordinates = response.data.results[0].geometry.location;
+        const map = new google.maps.Map(document.getElementById('map')!, {
+            center: coordinates,
+            zoom: 8
+          });
+        new google.maps.Marker({
+        position: coordinates,
+        map: map,
+        });
+        // console.log(response.data.results[0].geometry.location) 
+    }).catch(err => console.log('Error: ', err));
+    console.log(adress)
+}
+
+form.addEventListener('submit', eventHandler);
